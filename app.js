09356996157c77cc44c1e2d1ae59b35102481dd8
Naveena T S <naveenats@liveareacx.com>
2021-07-02 13:39:37 +0530

    Router and authentication added

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const cors = require("cors");
 
@@ -16,31 +15,3 @@ const port = process.env.PORT || 5050;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-=======
-const express = require("express");
-const cors = require("cors");
-
-require("dotenv").config();
-
-const app = express();
-app.use(cors());
-
-app.use(express.json());
-
-const port = process.env.PORT || 5050;
-
-app.get("/", (req, res) => {
-  console.log(req.query);
-  res.status(200).send("Hello  from endpoint");
-});
-
-app.post("/postData", (req, res) => {
-  console.log(req.body);
-  console.log(req.headers.authorization);
-  res.status(200).send("Hello  from endpoint");
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
->>>>>>> 564a32748964d0b36ff3a9fff311ce58811d7f54
